cfdb58c59bb7722ae2e63ea3f7ea473061432b02
wangdong <wangdong@zhuanzhuan.com>
2020-04-29 17:25:31 +0800

    桥接模式

--- ZLDesignPatterns-Demo/结构型模式/桥接模式/BridgePatternViewController.m
+++ ZLDesignPatterns-Demo/结构型模式/桥接模式/BridgePatternViewController.m
@@ -7,6 +7,10 @@
 //
 
 #import "BridgePatternViewController.h"
+#import "DirectionRemote.h"
+#import "TemperatureRemote.h"
+#import "HaierAirConditioner.h"
+#import "GeliAirConditioner.h"
 
 @interface BridgePatternViewController ()
 
@@ -17,16 +21,30 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+  
+    //海尔空调
+    HaierAirConditioner *haierAirConditioner =  [[HaierAirConditioner alloc] init];
+    
+    //控制风向
+    DirectionRemote *directionRemote = [[DirectionRemote alloc] init];
+    //让海尔空调往上吹风
+    directionRemote.airConditioner = haierAirConditioner;
+    [directionRemote up];
+    
+    //控制温度
+    TemperatureRemote *temperatureRemote = [[TemperatureRemote alloc] init];
+    //让海尔空调更冷
+    temperatureRemote.airConditioner = haierAirConditioner;
+    [temperatureRemote colder];
+    
+    //=====================================================================================
+    
+    //让格力空调往下吹热风
+    GeliAirConditioner *geliAirConditioner =  [[GeliAirConditioner alloc] init];
+    
+    directionRemote.airConditioner = geliAirConditioner;
+    [directionRemote down];
+    temperatureRemote.airConditioner = geliAirConditioner;
+    [temperatureRemote warmer];
 }
-
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
-
 @end
